e6fe9ed549f9aa506e0253ad0ead9c72c4b65d43
Millicent Aku Donkor <millicentaku23@gmail.com>
2024-11-09 14:07:48 +0000

    My python project

---
+++ main.py
@@ -0,0 +1,484 @@
+# # #This is my first comment
+# # #Variables - A variable is a container used to store element.
+# # #Examples of variables:
+# # name = "precious"
+# # #1.Rules for declaring a variable 
+# # #1.must not start with a digit. Example:
+# # #123Aba = "Ama"
+# # #2.Can start with underscore. Exaample:
+# # _Ama_ = "Abena"
+# # #3.Can start with a character/words/letters/and followed by a digit.
+# # #A12 = "BB Breaad"
+# # #4.Can start with aa character followed by an underscore followedd by 
+
+# # a = 7
+# # b = 2
+
+# # # addition
+# # print ('Sum: ', a + b)  
+
+# # # subtraction
+# # print ('Subtraction: ', a - b)   
+
+# # # multiplication
+# # print ('Multiplication: ', a * b)  
+
+# # # division
+# # print ('Division: ', a / b) 
+
+# # # floor division
+# # print ('Floor Division: ', a // b)
+
+# # # modulo
+# # print ('Modulo: ', a % b)  
+
+# # # a to the power b
+# # print ('Power: ', a ** b) 
+
+# # #Assinment Operator
+# # # assign 10 to a
+# # a = 10
+
+# # # assign 5 to b
+# # b = 5 
+
+# # # assign the sum of a and b to a
+# # a += b      # a = a + b
+
+# # print(a)
+
+# # # Output: 15
+
+# # #Addition Assignment
+# # a = 12
+# # b = 13
+# # a += b
+# # print(a)
+
+# # #Subtraction
+# # a = 10
+# # b = 5
+# # a -= b
+# # print(a)
+
+
+
+
+# # a = 5
+
+# # b = 2
+
+# # # equal to operator
+# # print('a == b =', a == b)
+
+# # # not equal to operator
+# # print('a != b =', a != b)
+
+# # # greater than operator
+# # print('a > b =', a > b)
+
+# # # less than operator
+# # print('a < b =', a < b)
+
+# # # greater than or equal to operator
+# # print('a >= b =', a >= b)
+
+# # # less than or equal to operator
+# # print('a <= b =', a <= b)
+
+
+
+# # Ghana = 30
+# # Togo = 10
+# # #AND
+# # print ( (Ghana < Togo) ) and( (Ghana > Togo) )
+
+# # #OR
+# # print ( (Ghana < Togo) ) or ( (Ghana > Togo) )
+
+# # print ( (Ghana <= Togo) ) and( (Ghana !=Togo) )
+
+
+# # #Not
+
+
+# #  # # # This is my first comment
+# # # # #Variables - A variable is a container used to store elements.
+# # # # #To declare a variable you need to introduce the assignment operator called Equal to.
+# # # # #We will look at examples of operators later then.
+# # # # #Note: We have Naming rules for declaring a variable. We will look at them one by one.
+# # # # #Examples of Variable: 
+# # # # #name = "Precious"
+# # # # #Rules for declaring a variable
+# # # # #1. Must not start with a Digit. Example:
+# # # # #123Aba = "Ama"
+# # # # #2. Can start with an underscore. Example:
+# # # # #_Ama_ = "Abena"
+# # # # #3.Can start with a character/words/letters and followed by a digit.
+# # # # #Example:
+# # # # #A12 = "BB Bread"
+# # # # #4. Can start with a character followed by and underscore followed by another character or you can choose not to add any character after the underscore.
+# # # # #Example:
+# # # # # tracy_boakye = "Robert"
+# # # # # Tracy_Boakye = "Kofi"
+# # # # # Tracy_ = "Wontumi"
+# # # # 5.# You can not use the keywords as a variable name. Example:
+# # # # # in = "Abena"
+# # # # # else = "Akosua"
+# # # # #With all these explains what identifiers are in python.
+
+# # # # #OPERATORS
+
+# # # # Hello = 1 // 2
+# # # # print (Hello)
+
+# # # a = 7 
+# # # b = 5
+
+# # # # addition
+# # # print (a + b) 
+
+# # # # Subtraction
+# # # print (a - b)
+
+# # # #multiplication
+# # # print (a * b)
+
+# # # #division 
+# # # print (a / b)
+
+# # # #floor division
+# # # print (a // b)
+
+# # # #Modulo
+# # # print (a % b)
+
+# # # #Power
+# # # print (a ** b)
+
+# # # # #Assignment Operators
+# # # # #We want assign 5 to A variable name
+# # # # name = 5
+
+# # # #Examples of Assignment Operators
+
+# # # # '=' Assignment Operator
+# # # #  += Addition Assignment
+# # # # -= Subtraction Assignment
+# # # # *= Multiplication Assignment
+# # # # /= Division Assignment
+# # # # %= Remainder Assignment
+# # # # **= Exponent assignment
+
+# # # # Addition Assignment:
+# # # # a = 12
+# # # # b = 13
+
+# # # # print (a)
+
+# # # # a += b
+
+# # # # print (a)
+
+# # # # Assignment operators assessment goes here
+
+
+# # # #Comparison Operators
+# # # # == - Is equal to
+# # # # != - Not equal to
+# # # # > - Greater Than
+# # # # < - Less than
+# # # # >= - Greater Than or Equal to
+# # # # <= - Less Than or Equal to
+
+# # # #Examples:
+# # # Ghana = 67
+# # # Nigeria = 12
+
+# # # # Is Equal to:
+# # # print (Ghana == Nigeria)
+
+# # # #Greater Than or Equal to:
+# # # print (Ghana >= Nigeria)
+
+# # # # Comparison operators assessment goes here
+
+
+# # # #Logical Operator
+# # # #Types of Logical operators:
+# # # # AND 
+# # # # OR
+# # # # NOT
+
+# # # #Examples :
+
+# # # #AND:
+# # # print ((Ghana > Nigeria) and (Nigeria > Ghana))
+
+# # # # OR:
+# # # print ((Ghana > Nigeria) or (Nigeria > Ghana))
+# # # print ((Ghana >= Nigeria) or (Nigeria != Ghana))
+
+# # # #NOT
+# # # print (not (Ghana > Nigeria))
+
+
+
+# # # print('string' ,'value')
+
+# # # # print with end whitespace
+# # # print('Good Morning!', end= ' ')
+
+# # # print('It is rainy today')
+
+# # # # print with end whitespace
+# # # print('Good Morning!')
+
+# # # print('It is rainy today')
+
+
+# # # print('New Year', 2023, 'See you soon!', sep= '. ')
+
+
+
+# # # weight = int(input('Please enter your weight:'))
+# # # print(weight)
+
+# # # Year = int(input('please enter your year'))
+# # # print(Year)
+
+# # # month = int(input('please enter your month'))
+# # # print(2024)
+
+
+
+# # # #Data types :
+# # # #Number Data Type: They are data types that are used to store numeric data values
+
+# # # # Types:
+# # # # int,float and complex
+# # # #Examples of int:2, -4, 5,10,25
+# # # #Examples  float : 0.65,15.8,3,142, 1.999
+# # # # Eg of complex:binary(0b1,10B01,0B11011,)hexadecimals(0XFB, 0X10),Octals(0o248,0o1o98)
+# # # #pratical exams
+ 
+# # # print(0b1101011)
+
+# # # print(0xfb + 0b10)
+
+# # # print(0o15)
+
+# # # print(0b11011001)
+
+
+# # print(0b1101011)  # prints 107
+
+# # print(0xBB + 0b10)  # prints 13
+
+# # print(0o15)  # prints 13
+
+# # print(0b11011001) # print 217
+
+
+# # import math
+
+# # print(math.pi)
+
+# # print(math.cos(math.pi))
+
+# # print(math.exp(10))
+
+# # print(math.log10(1000))
+
+# # print(math.sinh(1))
+
+# # print(math.factorial(6))
+
+# #  # a list of three elements
+# # ages = [19, 26, 29]
+# # print(ages)
+
+# # # Output: [19, 26, 29]
+
+
+# # languages = ['Python', 'Swift', 'C++']
+
+# # # Access the first element
+# # print(languages[0])   # Python
+
+# # # Access the third element
+# # print(languages[2])   # C++
+
+# # languages = ['Python', 'Swift', 'C++']
+
+# # # Access item at index 0
+# # print(languages[-1])   # C++
+
+# # # Access item at index 2
+# # print(languages[-3])   # Python
+
+# # fruits = ['apple', 'banana', 'orange']
+# # print('Original List:', fruits)
+
+# # # using append method 
+# # fruits.append('cherry')
+
+# # print('Updated List:', fruits)
+
+# # sets = [4,5,9,8,2,7,90,50]
+# # print(sets)
+# # sets.append('banana')
+# # print (set)
+# # sets.insert(9, 'koko')
+# # print(sets)
+
+# # colors = ['Red', 'Black', 'Green']
+# # print('Original List:', colors)
+
+# # # changing the third item to 'Blue'
+# # colors[2] = 'Blue'
+
+# # print('Updated List:', colors)
+
+# # #tuples:they are similar to lists
+# # #unlike the list its contents can not be changes
+
+# # # temperature=(12,'hot','cold','warm')
+# # # print('wet' in temperature)
+# # # print(temperature)
+
+# # # var=('hello')
+# # # print(var)
+
+
+# # #temperature=(1,2,3,4)
+# # #temperature=list(temperature)
+# # #temperature.insert(4,5)
+# # #temperature=tuple(temperature)
+# # print(temperature)
+
+# milli =(1,2,3)
+# milli  = list(milli)
+# milli.append(4)
+# milli = tuple(milli)
+# print(milli)
+
+# hello = "its"
+# print(hello[1])
+
+# hello = "its"
+# print(hello[-1])
+
+# hello = "that"
+# print(hello)
+
+# hello = "its"
+# print (hello[0])
+# print (hello)
+
+
+# hello = """it is a boy
+# it is me
+# it is good"""
+# print(hello)
+
+# hello = "it"
+# hello1 = "'s"
+# print(hello + hello1)
+# hello2 = "_me"
+# hello3 = " mario"
+# print(hello + hello1 +hello2 +hello3)
+
+# var = "good"
+#print(len(var))
+example = "he said,\ what\"s that"
+print (example)
+
+# escape double quotes
+example = "He said, \"What's there?\""
+
+# escape single quotes
+example = 'He said, "What\'s there?"'
+
+print(example)
+
+# Output: He said, "What's there?"
+
+name = 'Millicent'
+country = 'Ghana'
+continent = "Africa"
+
+print(f'{name} is from {country} in West {continent}')
+
+
+# create a set of mixed data types
+mixed_set = {'Hello', 101, -2, 'Bye', '24/1/1985'}
+print('Set of mixed data types:', mixed_set)
+
+even_numbers = {2,4,6,8,8,10,12,14,}
+print(even_numbers)
+
+numbers = {21, 34, 54, 12}
+print('Initial Set:',numbers)
+# using add() method
+numbers.add(32)
+print('Updated Set:', numbers) 
+
+prisoners = {3,6,9,12,15}
+print('initial set:',prisoners)
+#using add() method
+prisoners.add(18)
+print ('updated set :', prisoners)
+
+companies = {'Lacoste', 'Ralph Lauren'}
+tech_companies = ['apple', 'google', 'apple']
+# using update() method
+companies.update(tech_companies)
+print(companies)
+# Output: {'google', 'apple', 'Lacoste', 'Ralph Lauren'}
+
+languages = {'Swift', 'Java', 'Python'}
+print('Initial Set:',languages)
+# remove 'Java' from a set
+removedValue = languages.discard('Java')
+print('Set after remove():', languages)
+
+# first set
+A = {1, 3, 5}
+# second set
+B = {0, 2, 4}
+# perform union operation using |
+print('Union using |:', A | B)
+# perform union operation using union()
+print('Union using union():', A.union(B)) 
+
+set1 = {1,2,4}
+set2 = {2,5,10}
+print("union using(|):", set1| set2)
+print("union using(.union):", set1.union(set2))
+
+
+#Intersection of set
+print("intersection using (&):", set1 & set2)
+print("intersection using (.intersection):", set1.intersection (set2))
+
+
+# first set
+A = {2, 3, 5}
+# second set
+B = {1, 2, 6}
+# perform difference operation using &
+print('Difference using &:', A - B)
+# perform difference operation using difference()
+print('Difference using difference():', A.difference(B)) 
+
+
+m = {6,8,9}
+n = {3,4,5}
+print("difference using -:", m-n)
+print("difference using difference():", m.difference(n))
+
+m = {6,8,9}
+n = {3,4,5}
+print("difference using -:", n-m)
+print("difference using difference():", n.difference(m))
